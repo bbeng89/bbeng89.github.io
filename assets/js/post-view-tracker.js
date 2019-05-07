3dcf21dd70df0b9775ce0c39577cf6437c7b0486
Blake Bengtson <blake@bengtson.us>
2019-05-06 20:35:57 -0600

    Added JS to track the last photocount the user saw on the latest post so that I can highlight the count green if there are new photos

---
+++ assets/js/post-view-tracker.js
@@ -0,0 +1,35 @@
+var PostViewTracker = function(latestPostId, photoCount) {
+    var self = this;
+    self.latestPostId = latestPostId;
+    self.photoCount = photoCount;
+    self.key = 'latestPost';
+
+    var updateLastPostSeen = function() {
+        var post = {
+            id: self.latestPostId,
+            photoCount: self.photoCount
+        };
+        localStorage.setItem(self.key, JSON.stringify(post));
+    }
+
+    var getLastPostSeen = function() {
+        var post = localStorage.getItem(self.key);
+        if(post == null) return null;
+        return JSON.parse(post);
+    }
+
+    var hasSeenLatestPost = function() {
+        var post = getLastPostSeen();
+        return post != null && post.id == self.latestPostId;
+    }
+
+    var hasSeenAllLatestPostPhotos = function() {
+        var post = getLastPostSeen();
+        return hasSeenLatestPost() && post.photoCount == self.photoCount;
+    }
+
+    return {
+        hasSeenAllLatestPostPhotos: hasSeenAllLatestPostPhotos,
+        updateLastPostSeen: updateLastPostSeen
+    }
+}
